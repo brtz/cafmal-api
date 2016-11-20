87ffef9cf68dcdb69afe298f77b045654387471f
Nils Bartels <Nils Bartels>
2016-11-20 01:18:36 +0100

    [CHANGE] default to UTC; [CHANGE] fix missing kind on event;

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   enum severity: [:info, :warning, :critical, :error]
+  enum kind: [:check, :alert, :login]
 
   belongs_to :team
 end
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module CafmalApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.time_zone = 'UTC'
+    config.active_record.default_timezone = :utc
   end
 end
