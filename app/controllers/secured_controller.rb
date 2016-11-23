521c00d793a98b1b5361a209205d771fc57be5a7
Nils Bartels <nbartels@jimgordon.fritz.box>
2016-11-23 23:18:46 +0100

    [CHANGE] creative way of presenting fields with types;

--- app/controllers/secured_controller.rb
+++ app/controllers/secured_controller.rb
@@ -2,6 +2,7 @@ class SecuredController < ApplicationController
   before_action :authenticate_user
 
   def new
-    render json: "#{params[:controller]}".singularize.camelize.constantize.column_names
+    classname = "#{params[:controller]}".singularize.camelize.constantize
+    render json: classname.attribute_names.map {|n| [n.to_sym,classname.type_for_attribute(n).type]}.to_h
   end
 end
