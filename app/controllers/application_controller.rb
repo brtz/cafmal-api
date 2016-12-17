f0ebb40c97483138912843d4f401b916315d578e
Nils Bartels <n.bartels@bigpoint.net>
2016-12-17 11:58:19 +0100

    [CHANGE] consistent messages;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::API
   include Knock::Authenticable
 
   def index
-    render json: {IAm: "cafmal-api", version: CAFMAL_API_VERSION, pubkey: Rails.application.secrets.token_es256_pub}
+    render json: {IAm: "cafmal-api", Version: CAFMAL_API_VERSION, Pubkey: Rails.application.secrets.token_es256_pub}
   end
 
   rescue_from "AccessGranted::AccessDenied" do |exception|
-    render json: {IAm: "cafmal-api", version: CAFMAL_API_VERSION, message: "Authorization denied"}, status: 403
+    render json: {IAm: "cafmal-api", Version: CAFMAL_API_VERSION, Message: "Authorization denied"}, status: 403
   end
 end
