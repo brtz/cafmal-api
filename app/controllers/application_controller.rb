017e39adcbe61830d0fbd37016ae3e37e662d495
Nils Bartels <n.bartels@bigpoint.net>
2017-01-15 11:10:53 +0100

    [CHANGE] pubkey handling;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
   include Knock::Authenticable
 
   def index
-    render json: { IAm: 'cafmal-api', Version: CAFMAL_API_VERSION, Pubkey: Rails.application.secrets.token_es256_pub }
+    render json: { IAm: 'cafmal-api', Version: CAFMAL_API_VERSION, Pubkey: Knock.token_public_key.to_pem }
   end
 
   rescue_from 'AccessGranted::AccessDenied' do
