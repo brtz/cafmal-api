8d57eb1d6b088b9728ef7dbe5bee1d40a95fbd4a
nbartels <github@thes33k.de>
2017-01-23 01:08:04 +0100

    [CHANGE] replace unicorn with puma;

---
+++ config/puma.rb
@@ -0,0 +1,20 @@
+# config/puma.rb
+
+threads_min = ENV['CAFMAL-API_PUMA_THREADS_MIN'] || 8
+threads_max = ENV['CAFMAL-API_PUMA_THREADS_MAX'] || 32
+workers_max = ENV['CAFMAL-API_PUMA_WORKERS'] || 3
+ram = ENV['CAFMAL-API_PUMA_RAM'] || 1024
+
+threads threads_min.to_i, threads_max.to_i
+workers workers_max.to_i
+preload_app!
+
+before_fork do
+  require 'puma_worker_killer'
+
+  PumaWorkerKiller.enable_rolling_restart(1 * 3600)
+  PumaWorkerKiller.frequency = 30
+  PumaWorkerKiller.ram = ram.to_i
+
+  PumaWorkerKiller.start
+end
